e6c5074b45a5c52d07e9fdfccc322aecf6a5f0b5
roma1st <romastrozyuk21@gmail.com>
2024-04-08 00:07:16 +0300

    feat: added sorting

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <!-- Markup shared across all pages, ex: NavBar -->
+    <NuxtPage />
+
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import Product from "~/pages/Product.vue";
+</script>
+
+<template>
+  <Product/>
+</template>
+
+<style scoped>
+
+</style>
